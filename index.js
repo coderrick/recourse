0af04cd45b8d581590cf4bcce835abfdb117d848
Chul Ahn <scahn.x@gmail.com>
2019-10-27 15:28:41 -0400

    latest

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ var express = require("express");
 var cors = require("cors");
 var app = express();
 app.use(cors());
-app.use(cors({ origin: "http://localhost:3000/", credentials: true }));
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET, POST, PUT, PATCH, POST, DELETE, OPTIONS",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+    maxAge: 86400,
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
 
 app.get("/", function(req, res) {
   res.sendfile("./Middleware/index.html");
